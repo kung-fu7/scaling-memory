0540ea3ecf115dac5efbddba79c4659c09e1d687
unknown <sujitkumarbhutiaskb@gmail.com>
2022-08-04 10:54:25 +0530

    age.cpp file added

---
+++ age.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"enter the age";
+    cin>>age;
+    if(age>=12 && age<=50)
+    {
+        cout<<"young";
+    }
+    else
+    {
+        cout<<"not young";
+    }
+ return 0;
+}
